c85195d08f53c6f0ab006a483e50bbbd522fde63
wenxuan-pan <panwenxuan42@gmail.com>
2023-11-15 15:21:01 +1100

    navbar and sidebar responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import MainContainer from "./components/MainContainer"
+import NavBar from "./components/NavBar"
+import SideBar from "./components/SideBar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const toggleSidebar = (): void => {
+    setIsSidebarOpen((prev: boolean) => !prev)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="3xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs underline">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="antialiased bg-gray-50 dark:bg-gray-900">
+      <NavBar
+        title="Task Hatch"
+        logo="/task-hatch-logo.png"
+        toggleSidebar={toggleSidebar}
+      />
+      <SideBar isSidebarOpen={isSidebarOpen} />
+      <MainContainer
+        toggleSidebar={toggleSidebar}
+        isSidebarOpen={isSidebarOpen}
+      />
+    </div>
   )
 }
 
---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+// props types
+interface MainContainerProps {
+  toggleSidebar: () => void
+  isSidebarOpen: boolean
+}
+
+const MainContainer: React.FC<MainContainerProps> = ({
+  toggleSidebar,
+  isSidebarOpen
+}) => {
+  return (
+    <>
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-30 transition-opacity duration-300 ease-in-out"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+      <main className="p-4 md:ml-64 h-auto pt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+          <div className="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
+        </div>
+        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
+        <div className="grid grid-cols-2 gap-4 mb-4">
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+        </div>
+        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default MainContainer
