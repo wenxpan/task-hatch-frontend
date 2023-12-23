ab5c05026573fb0f151a95f83a1923c9827236ff
wenxuan-pan <panwenxuan42@gmail.com>
2023-12-23 21:19:50 +1100

    finished status group in view task

--- src/components/StatusGroup.tsx
+++ src/components/StatusGroup.tsx
@@ -33,13 +33,29 @@ const StatusGroup: React.FC<StatusGroupProps> = ({ task }) => {
   const statusOptions = [
     {
       name: "archived",
-      color: "bg-amber-800",
+      color: "bg-amber-800 hover:bg-amber-800",
       icon: ArchiveSVG
     },
-    { name: "snoozed", color: "bg-gray-600", icon: SnoozeSVG },
-    { name: "in progress", color: "bg-blue-600", icon: PinLineSVG },
-    { name: "prioritised", color: "bg-yellow-600", icon: PinFillSVG },
-    { name: "completed", color: "bg-green-600", icon: TickSVG }
+    {
+      name: "snoozed",
+      color: "bg-gray-600",
+      icon: SnoozeSVG
+    },
+    {
+      name: "in progress",
+      color: "bg-blue-600",
+      icon: PinLineSVG
+    },
+    {
+      name: "prioritised",
+      color: "bg-yellow-600",
+      icon: PinFillSVG
+    },
+    {
+      name: "completed",
+      color: "bg-green-600",
+      icon: TickSVG
+    }
   ].map((option) => ({
     ...option,
     name: option.name as TaskStatus
@@ -48,13 +64,15 @@ const StatusGroup: React.FC<StatusGroupProps> = ({ task }) => {
   const StatusButton: React.FC<StatusButtonProps> = ({ name, color, Icon }) => {
     const isSelected = status === name
     const baseStyles =
-      "inline-flex items-center rounded-lg whitespace-nowrap px-4 py-2 text-sm font-medium hover:text-white hover:bg-gray-800 focus:z-10 focus:ring-2 focus:ring-gray-500"
-    const selectedStyles = isSelected ? `text-white ${color}` : "text-gray-900"
+      "inline-flex items-center rounded-lg whitespace-nowrap px-4 py-2 text-sm font-medium hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500"
+    const buttonStyles = `${baseStyles} ${
+      isSelected ? `text-white ${color}` : "text-gray-900"
+    }  hover:${color}`
 
     return (
       <button
         type="button"
-        className={`${baseStyles} ${selectedStyles}`}
+        className={buttonStyles}
         onClick={() => onChangeStatus(name)}
       >
         <Icon className="w-3 h-3 mr-2" />
