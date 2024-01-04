355575a0e54ea1f8e8e867f7ace91a08c5e7873c
wenxuan-pan <panwenxuan42@gmail.com>
2024-01-04 13:30:34 +1100

    moved toastcontainer; edited handle error logic

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,6 +1,19 @@
 import { toast } from "react-toastify"
 
-export const handleError = (e: Error, msg = e.message) => {
-  console.error(msg)
-  toast.error(msg)
+export const handleError = (
+  error: any,
+  defaultMessage = "An error occurred, please try again"
+) => {
+  let message = defaultMessage
+
+  // Check if the error is an Axios error with a response and a custom message
+  if (error.response && error.response.data && error.response.data.error) {
+    message = error.response.data.error
+  } else if (error.message) {
+    // Fallback to the generic error message
+    message = error.message
+  }
+
+  console.error(message)
+  toast.error(message)
 }
